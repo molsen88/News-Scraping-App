e62f1c3b660d62f535bdd558f0a47e3984abda06
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-06-22 20:34:23 -0600

    post

--- public/app.js
+++ public/app.js
@@ -50,7 +50,7 @@ $( document ).on( "click", "#savenote", function () {
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax( {
         method: "POST",
-        url: "/articles/" + thisId,
+        url: "/" + thisId,
         data: {
             // Value taken from title input
             title: $( "#titleinput" ).val(),
